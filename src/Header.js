589d7b4b43077a988a62a7384003cf87d4c4aaef
Max Komar <silencecut@gmail.com>
2023-04-08 16:15:24 +0300

    resolve problem

--- src/Header.js
+++ src/Header.js
@@ -11,6 +11,7 @@ import Home from "./Pages/Home";
 import Contacts from "./Pages/Contacts";
 import About from "./Pages/About";
 import Blog from "./Pages/Blog";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import avatar from "./img/avatar.png"
 
@@ -18,7 +19,39 @@ export default class Header extends Component {
   render() {
     return (
           <>
-          <Router>
+          <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">
+                       <img
+                       src={avatar}
+                       height="30"
+                       width="30"
+                       className="d-inline-block align-top"
+                       alt = "Logo"
+                       /> React Site
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav claccName="me-auto">
+                            <Nav.Link href="/"> Home </Nav.Link>
+                            <Nav.Link href="/about"> About us </Nav.Link>
+                            <Nav.Link href="/contacts"> Contacts </Nav.Link>
+                            <Nav.Link href="/blog"> Blog </Nav.Link>
+                        </Nav>
+                        <Form className="d-flex">
+                            <FormControl
+                                type ="text"
+                                placeholder="Search"
+                                className="me-sm-3"
+                            />
+                            <Button variant="outline-info">Search</Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+              
+              <Router>
             <Routes>
                 <Route path="/" element={<Home />}/>
                 <Route path="/about" element={<About/>}/>
@@ -26,27 +59,9 @@ export default class Header extends Component {
                 <Route path="/blog" element={<Blog/>}/>
             </Routes>
           </Router>
-
-          <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
-             <Container>
-                <Navbar.Brand href="/">
-                <img src={avatar} height="30" width="30" className="d-inline-block align-top" alt = "Logo" /> React Site 
-                </Navbar.Brand> 
-                <Navbar.Toggle aria-controls="responsive-navbar-nav"/> 
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav claccName="me-auto"> <Nav.Link href="/"> Home </Nav.Link>
-                      <Nav.Link href="/about"> About us </Nav.Link>
-                      <Nav.Link href="/contacts"> Contacts </Nav.Link>
-                      <Nav.Link href="/blog"> Blog </Nav.Link>
-                    </Nav>
-                    <Form className="d-flex">
-                      <FormControl type ="text" placeholder="Search" className="me-sm-3" />
-                      <Button variant="outline-info">Search</Button>
-                    </Form>
-                  </Navbar.Collapse>
-              </Container>
-           </Navbar>
+           
            </>
+           
     )
   }
 }
