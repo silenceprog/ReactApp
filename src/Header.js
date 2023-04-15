80a4d8579292ce22506addf7869268eca71c61b7
Max Komar <silencecut@gmail.com>
2023-04-15 21:45:50 +0300

    make lab4

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
+import React, { useState,useEffect } from "react";
 import { Navbar,
-   Nav,
-   FormControl,
-   Container,
-   Form,
-   Button
+  Nav,
+  FormControl,
+  Container,
+  Form,
+  Button,
+  Modal
    } from "react-bootstrap";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./Pages/Home";
@@ -15,8 +16,61 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import avatar from "./img/avatar.png"
 
-export default class Header extends Component {
-  render() {
+export default function Header(){
+        const [show, setShow] = React.useState(false);
+    
+        const handleClose = () => setShow(false);
+        const handleShow = () => setShow(true);
+        const [email, setEmail] = useState('')
+        const [password, setPassword] = useState('')
+        const [emailDirty, setEmailDirty] = useState( false)
+        const [passwordDirty, setPasswordDirty] = useState( false)
+        const [emailError, setEmailError] = useState( 'Email не може бути порожнім')
+        const [passwordError, setPasswordError] = useState( 'Пароль не може бути порожнім')
+        const [formValid, setFormValid] = useState(false)
+
+        const emailHandler = (e) => {
+            setEmail(e.target.value)
+            const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+            if (!re.test(String(e.target.value.toLowerCase()))) {
+                setEmailError('Некоректний email')
+            } else {
+                setEmailError('')
+            }
+        }
+
+        const passwordHandler = (e) => {
+            setPassword(e.target.value)
+            if (e.target.value.length < 3 || e.target.length > 8) {
+                setPasswordError('Пароль повинен мати не менше 3 і не більше 8 символів')
+                if (!e.target.value) {
+                    setPasswordError('Пароль не може бути порожнім')
+                }
+            } else {
+                setPasswordError('')
+            }
+        }
+        
+        const blurHandler = (e) => {
+            switch (e.target.name) {
+                case 'email':
+                    setEmailDirty(true)
+                    break
+                case 'password':
+                    setPasswordDirty(true)
+                    break
+            }
+        }
+        
+        useEffect ( () => {
+            if (emailError || passwordError) {
+                setFormValid(false)
+            } else {
+                setFormValid(true)
+            }
+        },[emailError, passwordError])
+        
+
     return (
           <>
           <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
@@ -45,10 +99,46 @@ export default class Header extends Component {
                                 className="me-sm-3"
                             />
                             <Button variant="outline-info">Search</Button>
+                            <Button className="ms-2" onClick={handleShow}>Login</Button>
                         </Form>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Log in</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="fromBasicEmail">
+                            <Form.Label>Email Address</Form.Label>
+                            {(emailDirty && emailError) && <div style={{color:"red"}}>{emailError}</div>}
+                            <Form.Control onChange={e => emailHandler(e)} name="email" value={email}
+                             onBlur={e => blurHandler(e)} type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group controlId="fromBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            {(passwordError && passwordDirty) && <div style={{color:"red"}}>{passwordError}</div>}
+                            <Form.Control onChange={e => passwordHandler(e)} name="password" 
+                            value={password} onBlur={e => blurHandler(e)} type="password" placeholder="Enter password">
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId="fromBasicCheckbox">
+                            <Form.Check type="checkbox" label="Remember me"/>
+                        </Form.Group>
+
+                        <Button disabled={!formValid} variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Modal.Body>
+            </Modal>
 
               
               <Router>
@@ -61,7 +151,7 @@ export default class Header extends Component {
           </Router>
            
            </>
+    
            
     )
-  }
 }
